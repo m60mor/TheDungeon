a30f2b6592ca96311175977522e9a85542088c77
unknown <m60mor@gmail.com>
2024-04-02 14:45:55 +0200

    Setting up room generator

--- map_generator.gd
+++ map_generator.gd
@@ -3,31 +3,46 @@ class_name MapGenerator
 
 const DIRECTIONS = [Vector2.RIGHT, Vector2.UP, Vector2.LEFT, Vector2.DOWN]
 
+var tile_map : TileMap = null
 var position = Vector2.ZERO
 var direction = Vector2.RIGHT
 var borders = Rect2()
 var step_history = []
 var steps_since_turn = 0
+var base_room_size = [7, 9]
+var room_size : float = 7
+var prev_room_size : float = 7
 
-func _init(starting_position, new_borders):
-	assert(new_borders.has_point(starting_position))
+func _init(starting_position, new_borders, new_tile_map):
+	#assert(new_borders.has_point(starting_position))
 	step_history.append(position)
 	borders = new_borders
+	tile_map = new_tile_map
 
 func walk(steps):
+	create_room(position)
+	tile_map.set_cell(0, position, 2, Vector2(0, 0))
 	for step in steps:
-		if randf() <= 0.25 or steps_since_turn >= 4:
-			change_direction()
+		change_direction()
 			
 		if step():
 			step_history.append(position)
+			tile_map.set_cell(0, position, 2, Vector2(0, 0))
+			create_room(position)
 		else:
 			change_direction()
 	return step_history
 	
 func step():
-	var target_position = position + direction
-	if borders.has_point(target_position):
+	prev_room_size = room_size
+	room_size = base_room_size[randi() % 2]
+	var target_position = position + (direction * (ceil(room_size / 2) + ceil(prev_room_size / 2) + 2))
+	if borders.has_point(target_position) and tile_map.get_cell_source_id(0, target_position) == -1:
+		var target_vector = target_position - position
+		print(target_vector)
+		for x in target_vector.x:
+			print(x)
+			tile_map.set_cell(0, Vector2(x, 0) + position, 2, Vector2(0, 0))
 		steps_since_turn += 1
 		position = target_position
 		return true
@@ -40,8 +55,15 @@ func change_direction():
 	directions.erase(direction)
 	directions.shuffle()
 	direction = directions.pop_front()
-	print(position)
-	print(borders)
 	while not borders.has_point(position + direction):
-		print(position + direction)
 		direction = directions.pop_front()
+		
+func create_room(position):
+	var size = Vector2(room_size, room_size)
+	var top_left = (position - size/2).ceil()
+	for y in size.y:
+		for x in size.x:
+			var new_step = top_left + Vector2(x, y)
+			if borders.has_point(new_step):
+				step_history.append(new_step)
+				tile_map.set_cell(0, new_step, 2, Vector2(0, 0))
--- scenes/game/level_1.gd
+++ scenes/game/level_1.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var borders = Rect2(-1, -1, 38, 21)
+var borders = Rect2(-100, -100, 200, 200)
 
 @onready var tile_map = $TileMap
 
@@ -9,9 +9,7 @@ func _ready():
 	
 	
 func generate_level():
-	var walker = MapGenerator.new(Vector2(19, 11), borders)
-	var map = walker.walk(500)
+	var walker = MapGenerator.new(Vector2(100, 100), borders, tile_map)
+	var map = walker.walk(20)
 	walker.queue_free()
-	for location in map:
-		tile_map.set_cell(0, location, -1)
 	#tile_map.update_bitmask_region(borders.position, borders.end)
